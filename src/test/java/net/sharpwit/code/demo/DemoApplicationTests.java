a49aeba7b1477763a7ce945b57c01c7aac5e2fdb
Kenny Younger <kyounger@gmail.com>
2020-09-24 15:07:31 -0500

    init

---
+++ src/test/java/net/sharpwit/code/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.sharpwit.code.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
